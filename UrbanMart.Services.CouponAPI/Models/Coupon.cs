8e6c8db128ae77ae2e11ff0525b00920d951f49a
Bhushan <baviskar615@gmail.com>
2025-12-05 23:41:05 +0530

    Initializing Coupon Project

---
+++ UrbanMart.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,13 @@
+﻿namespace UrbanMart.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public Guid Id { get; set; }
+        public required string Code{ get; set; }
+        public double Discount{ get; set; }
+        public bool IsDeprecated{ get; set; }
+        public bool IsActive{ get; set; }
+        public DateTime CreatedOn{ get; set; }
+        public DateTime ModifiedOn{ get; set; }
+    }
+}
